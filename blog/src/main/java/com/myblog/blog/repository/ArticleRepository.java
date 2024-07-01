e6f679eb2baea2df46b320abfb2bb8e8ddb0454e
Julie Raymond <julie.raymond@pichet.com>
2024-07-01 14:44:56 +0200

    quest04 done

--- blog/src/main/java/com/myblog/blog/repository/ArticleRepository.java
+++ blog/src/main/java/com/myblog/blog/repository/ArticleRepository.java
@@ -4,6 +4,14 @@ import com.myblog.blog.model.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    List<Article> findByTitle(String title);
+    List<Article> findByContentContaining(String content);
+    List<Article> findByCreatedAtAfter(LocalDateTime date);
+    List<Article> findTop5ByOrderByCreatedAtDesc();
 }
